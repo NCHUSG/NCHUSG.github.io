e6be2c789a0fdc12c83b05eed8fa83a0565c22ef
liao2000 <liao2000@livemail.tw>
2021-12-20 12:30:46 +0800

    Add padding

--- lib/pages/links.dart
+++ lib/pages/links.dart
@@ -17,24 +17,30 @@ class _LinksPageState extends State<LinksPage> {
         body: Scrollbar(
             isAlwaysShown: true,
             child: SingleChildScrollView(
-                child: Center(
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                  Text(
-                    '相關連結',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                  const LinkedCard(
-                      text: '會長令、各部門公告', url: 'https://reurl.cc/W3zM5y'),
-                  const LinkedCard(
-                      text: '第27屆總預算案', url: 'https://reurl.cc/8n7KbR'),
-                  const LinkedCard(
-                      text: '第27屆總決算案', url: 'https://reurl.cc/R0gXnx'),
-                  const LinkedCard(
-                      text: '學權申訴系統', url: 'https://reurl.cc/gzaRbL'),
-                  const LinkedCard(
-                      text: '社團海報張貼申請', url: 'https://reurl.cc/kLapEG '),
-                ])))));
+                child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Center(
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                          Text(
+                            '相關連結',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                          const LinkedCard(
+                              text: '資訊公開專區', url: 'https://reurl.cc/6Dnyx5'),
+                          const LinkedCard(
+                              text: '會長令、各部門公告',
+                              url: 'https://reurl.cc/W3zM5y'),
+                          const LinkedCard(
+                              text: '第27屆總預算案', url: 'https://reurl.cc/8n7KbR'),
+                          const LinkedCard(
+                              text: '第27屆總決算案', url: 'https://reurl.cc/R0gXnx'),
+                          const LinkedCard(
+                              text: '學權申訴系統', url: 'https://reurl.cc/gzaRbL'),
+                          const LinkedCard(
+                              text: '社團海報張貼申請',
+                              url: 'https://reurl.cc/kLapEG '),
+                        ]))))));
   }
 }
