84aff1bbf5ab39348cc533a505e5c1cd0701b4bb
liao2000 <liao2000@livemail.tw>
2021-12-21 19:08:40 +0800

    Update page/about & components/card

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -2,12 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LinkedCard extends StatelessWidget {
+  /// Creates a card contains link information.
+  ///
+  /// The [Text] is the title of the card.
+  ///
+  /// The [urlHint] is the subtitle of the card if [urlHint] is non-null,
+  /// else the subtitle is [url] instead.
+  const LinkedCard(
+      {Key? key,
+      required this.text,
+      required this.url,
+      this.urlHint,
+      this.width = 500})
+      : super(key: key);
   final double width;
   final String text;
   final String url;
-  const LinkedCard(
-      {Key? key, required this.text, required this.url, this.width = 500})
-      : super(key: key);
+  final String? urlHint;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -19,7 +30,7 @@ class LinkedCard extends StatelessWidget {
         maxWidth: 200,
         child: ListTile(
           title: Text(text),
-          subtitle: Text(url),
+          subtitle: Text(urlHint ?? url),
           trailing: TextButton(
             child: const Text('前往'),
             onPressed: () {
--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:io_github_nchusg/components/drawer.dart';
+import 'package:io_github_nchusg/components/card.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -8,17 +9,58 @@ class AboutPage extends StatefulWidget {
 }
 
 class _AboutPageState extends State<AboutPage> {
+  double width = 500;
+  final scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('關於學生會')),
       drawer: MyDrawer(),
-      body: Center(
-        child: Text(
-          '關於學生會',
-          style: Theme.of(context).textTheme.headline4,
-        ),
-      ),
+      body: Scrollbar(
+          isAlwaysShown: true,
+          controller: scrollController,
+          child: SingleChildScrollView(
+              controller: scrollController,
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 10.0),
+                  child: Center(
+                      child: SizedBox(
+                          width:
+                              (MediaQuery.of(context).size.width > width * 1.5)
+                                  ? width
+                                  : double.infinity,
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('聯絡方式',
+                                    style:
+                                        Theme.of(context).textTheme.headline6),
+                                const SizedBox(height: 5),
+                                const LinkedCard(
+                                    text: 'Email',
+                                    urlHint: 'nchusg.27@gmail.com',
+                                    url: 'mailto:nchusg.27@gmail.com'),
+                                const LinkedCard(
+                                    text: 'Facebook 粉專',
+                                    urlHint: '@nchusg',
+                                    url: 'https://www.facebook.com/nchusg/'),
+                                const LinkedCard(
+                                    text: 'Instagram',
+                                    urlHint: '@nchu_sg27',
+                                    url:
+                                        'https://www.instagram.com/nchu_sg27/'),
+                                const LinkedCard(
+                                    text: 'Github',
+                                    urlHint: '@NCHUSG',
+                                    url: 'https://github.com/NCHUSG'),
+                                Text('值班時間',
+                                    style:
+                                        Theme.of(context).textTheme.headline6),
+                                const SizedBox(height: 5),
+                                Text('雲平樓1樓F7 週一~週五 12:00~13:00')
+                              ])))))),
     );
   }
 }
